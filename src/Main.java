eb934c9003483c2f0e4e87e5ed5ac04f71961277
gudentc <135129462+gudentc@users.noreply.github.com>
2023-06-04 15:06:26 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@ public class Main
         System.out.println("Credit Card Balance$" + String.format("%,.2f", ccBalance));
         System.out.println("CC Interest Rate " + (INTEREST_RATE * 100) + "%");
 
-        interestDue = ccBalance * INTEREST_RATE/12;
-        ccBalance = ccBalance + (ccBalance * INTEREST_RATE);
+        interestDue = ccBalance * (INTEREST_RATE/12);
+        ccBalance = ccBalance + (ccBalance * INTEREST_RATE/12);
 
         // After 1 Month
         System.out.println("Interest due after 1 month is $" + String.format("%,.2f", interestDue));
         System.out.println("New Balance is $" + String.format("%,.2f",  ccBalance));
 
-        interestDue = ccBalance * (INTEREST_RATE);
-        ccBalance = ccBalance + (ccBalance * (INTEREST_RATE));
+        interestDue = ccBalance * (INTEREST_RATE/12);
+        ccBalance = ccBalance + (ccBalance * (INTEREST_RATE/12));
 
         // After 2 months
         System.out.println("New Balance after 2 months is $" + String.format("%,.2f", interestDue));
